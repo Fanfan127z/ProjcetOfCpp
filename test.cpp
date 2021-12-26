ba323cd7e64c56f74eedb41e400b2c73f9b4d4c7
lzf <1160273522@qq.com>
2021-12-26 13:39:21 +0800

    lalal C2 revise the codes of the test.cpp

--- test.cpp
+++ test.cpp
@@ -5,5 +5,6 @@ int main(void){
 	cout<<"Hello,github world!<<endl;
 	return 0;
 }
-//Customer two revise the codes of the test.cpp okk yeah!
+//Customer two revise the codes of the test.cpp okk yeah
+//kslalalalla!
 
